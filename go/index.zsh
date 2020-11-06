9187656568b19f309e606c94b5a3f72bfc2df739
Luca Zambarda <luca@blokur.com>
2020-11-06 11:06:10 +0000

    Update etcd script Update go version Update iterm preferences

--- go/index.zsh
+++ go/index.zsh
@@ -2,12 +2,13 @@ export GOPATH=$HOME/go
 export GO111MODULE=auto
 export GOPROXY=direct # For compatibility with gimme
 export GOSUMDB=off    # For compatibility with gimme
-export GIMME_DEFAULT_GO_VERSION=1.14
+export GIMME_DEFAULT_GO_VERSION=1.14.1
 
 setupgimme() {
     if [ -z ${GIMME_DEFAULT_GO_VERSION+x} ]; then
         echo "dotfiles: GIMME_DEFAULT_GO_VERSION not set, skipping\n"
     else
+    	echo "dotfiles: Downloading go ${GIMME_DEFAULT_GO_VERSION}..."
         eval "$(gimme $GIMME_DEFAULT_GO_VERSION)" 2>/dev/null
     fi
 }
--- kubernetes/path.zsh
+++ kubernetes/path.zsh
@@ -1,6 +1,5 @@
 # etcd section
-if [ -d /tmp/etcd-download-test ]; then
-    export PATH="/tmp/etcd-download-test/:$PATH"
-else
-    echo "dotfiles: etcd not installed, see https://github.com/etcd-io/etcd/releases"
+if test ! $(which etcd); then
+	echo "dotfiles: etcd not installed, see https://github.com/etcd-io/etcd/releases"
+	echo "dotfiles: brew install etcd?"
 fi
