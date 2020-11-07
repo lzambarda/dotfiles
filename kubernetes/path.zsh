beb19d810e9333a443859079e6c0185d9e9b896d
Luca Zambarda <luke.zambarda@gmail.com>
2020-11-07 11:21:41 +0000

    Fix etcd test

--- kubernetes/path.zsh
+++ kubernetes/path.zsh
@@ -1,5 +1,5 @@
 # etcd section
-if test ! $(which etcd); then
+if [ -z "$(which etcd)" ]; then
 	echo "dotfiles: etcd not installed, see https://github.com/etcd-io/etcd/releases"
 	echo "dotfiles: brew install etcd?"
 fi
