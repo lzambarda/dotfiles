f9469c86b2a3ae2d5de00dbba2911699f3a8d380
Luca Zambarda <luca.zambarda@posteo.net>
2021-10-16 12:22:27 +0100

    feat: add go new version installer

--- docker/index.zsh
+++ docker/index.zsh
@@ -1,1 +1,5 @@
 export DOCKER_BUILDKIT=1
+
+docker_stopall() {
+	docker stop $(docker ps -q)
+}
--- go/index.zsh
+++ go/index.zsh
@@ -2,18 +2,36 @@ export GOPATH=$HOME/go
 export GO111MODULE=on
 export GOPROXY=direct # For compatibility with gimme
 export GOSUMDB=off    # For compatibility with gimme
-export GIMME_DEFAULT_GO_VERSION=1.16.4
+export WANTED_GO_VERSION=1.17.2
 
 setupgimme() {
-    if [ -z ${GIMME_DEFAULT_GO_VERSION+x} ]; then
+    if [ -z ${WANTED_GO_VERSION+x} ]; then
         echo "dotfiles: GIMME_DEFAULT_GO_VERSION not set, skipping\n"
     else
         # Too slow for now
-    	# if [ -z "$(gimme list &>/dev/null | grep $GIMME_DEFAULT_GO_VERSION)" ]; then
-    	#  	echo "dotfiles: Downloading go ${GIMME_DEFAULT_GO_VERSION}..."
+    	# if [ -z "$(gimme list &>/dev/null | grep $WANTED_GO_VERSION)" ]; then
+    	#  	echo "dotfiles: Downloading go ${WANTED_GO_VERSION}..."
     	# fi
-        eval "$(gimme $GIMME_DEFAULT_GO_VERSION)" 2>/dev/null
+        eval "$(gimme $WANTED_GO_VERSION)" 2>/dev/null
     fi
+}
+
+checkgo() {
+    latest_version=$(gimme --known | tail -n1)
+    if [ "$latest_version" = "$WANTED_GO_VERSION" ]; then
+        return 0
+    fi
+    read "choice?Go version $latest_version is available. Do you want to install it? (y/n): "
+    case "$choice" in
+        y|Y|yes|YES ) echo "< yes";;
+        n|N|no|NO ) echo "< no"; return 0;;
+        * ) echo "Huh?"; return 0;;
+    esac
+    # Update configfile
+    sed "s/WANTED_GO_VERSION=$WANTED_GO_VERSION/WANTED_GO_VERSION=$latest_version/" ~/dotfiles/go/index.zsh | tee ~/dotfiles/go/index.zsh > /dev/null
+    echo "Config updated, installing..."
+    export WANTED_GO_VERSION=$latest_version
+    setupgimme
 }
 
 gocover () {
@@ -22,3 +40,4 @@ gocover () {
         go tool cover -html=coverage.out
         rm -rf ./coverage.out
 }
+
