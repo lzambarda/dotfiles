e49491e888ae671ce515299783a059317c006dbd
Luca Zambarda <luca@blokur.com>
2024-04-18 16:19:29 +0100

    feat(go): improve first time installation

--- go/index.zsh
+++ go/index.zsh
@@ -1,8 +1,15 @@
 checkgo() {
     goroot="/usr/local"
     version=$(go version 2>/dev/null | cut -d' ' -f 3)
+    if [ -z "$version" ]; then
+        version="no go :("
+    fi
     latest_version=$(curl -fsSL "https://golang.org/VERSION?m=text" | head -n1)
     if [ "$latest_version" = "$version" ]; then
+        return 0
+    fi
+    # Silently exit if an error occurred
+    if [ -z "$last_version" ]; then
         return 0
     fi
     read "choice?Go version $latest_version is available. You have $version, do you want to install it? (y/n): "
@@ -11,11 +18,21 @@ checkgo() {
         n|N|no|NO ) echo "< no"; return 0;;
         * ) echo "Huh?"; return 0;;
     esac
-    release_file="${latest_version}.darwin-amd64.tar.gz"
+    # Assert directory first
+    cusr=$(whoami)
+    if [ ! -d "$goroot/go" ]; then
+        echo "Creating $goroot/go..."
+        sudo mkdir "$goroot/go"
+    fi
+    if [ ! -O "$goroot/go" ]; then
+        echo "$goroot/go needs to be owned by $cusr, chowning it..."
+        sudo chown $cusr "$goroot/go"
+    fi
+    # Download go version
+    release_file="$latest_version.darwin-arm64.tar.gz"
     curr=$PWD
     tmp=$(mktemp -d)
     cd $tmp || exit 1
-
     echo "Downloading https://go.dev/dl/$release_file ..." 
     curl -OL https://go.dev/dl/$release_file
     echo "Uninstalling current go..."
